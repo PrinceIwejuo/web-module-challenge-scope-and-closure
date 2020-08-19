1d6a36358632f1373f7c4439e4cac9caf8e6e360
Prince Iwejuo <princeiwejuo@gmail.com>
2020-08-19 17:38:35 -0400

    ScopesAndClosureProject

--- index.js
+++ index.js
@@ -28,7 +28,11 @@ function processFirstItem(stringList, callback) {
  * 
  * 1. What is the difference between counter1 and counter2?
  * 
+ * - The difference between counter 1 and counter2 is that counter 1 is asking for the program to return a new function that will return the count variable 1 number higher. While counter2 is asking for a return of the count variable 1 number higher also, but within the orignal function.
+ * 
  * 2. Which of the two uses a closure? How can you tell?
+ * 
+ * -  The first counter uses a closure and you can tell because the inner scope refrences variables in its outter scope.
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
@@ -56,11 +60,14 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
+function inning(){
+  return Math.floor(Math.random() * 3);
 
-    /*Code Here*/
+  }
+  
+  console.log(inning());
 
-}
+  
 
 /* Task 3: finalScore()
 
@@ -76,12 +83,20 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(cb, innings){
+  let Home = 0;
+  let Away = 0;
+  for(let i =0; i <= innings; i++){
+    Home += cb()
+    Away += cb()
 
-  /*Code Here*/
-
+     }
+return{
+  home: Home,
+  away: Away 
 }
-
+};
+console.log(finalScore(inning, 9));
 /* Task 4: 
 
 Create a function called `scoreboard` that accepts the following parameters: 
@@ -103,8 +118,23 @@ and returns the score at each pont in the game, like so:
 Final Score: awayTeam - homeTeam */
 
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(finalScore, inning, number) {
+  let Home = 0;
+  let Away = 0;
+    for(let i = 1; i <= number; i++){
+      Home += finalScore(inning, i).home;
+      Away += finalScore(inning, i).away;
+      if(i == 1){console.log(`${i}st inning ${Away}-${Home}`)}
+      else if(i == 2) {console.log(`${i}nd inning ${Away}-${Home}`)}
+      else if (i == 3) {console.log(`${i}rd inning ${Away}-${Home}`)}
+      else{
+        console.log(`${i}th inning ${Away}-${Home}`)
+      }
+
+    }
+ return `final score ${Away}-${Home}`
 }
+
+console.log(scoreboard(finalScore, inning, 9));
 
 
